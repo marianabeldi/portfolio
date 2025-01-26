2dc1bf5d5274be97b34201013c43058a191231c7
Mariana Beldi <mbeldi@gmail.com>
2025-01-26 09:14:47 -0300

    styles

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
   })
 
     // Add a filter called "excerpt" to create a shortened version of a text
-    eleventyConfig.addFilter('excerpt', function(text, length = 10) {
+    eleventyConfig.addFilter('excerpt', function(text, length = 100) {
       if (text.length > length) {
         return text.substring(0, length) + '...';
       }
