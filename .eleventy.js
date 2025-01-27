a00bebac56d3e67986e60ed5be7ea84a9a3775d3
Mariana Beldi <mbeldi@gmail.com>
2025-01-27 20:53:05 -0300

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,53 @@
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  })
+  });
 
-    // Add a filter called "excerpt" to create a shortened version of a text
-    eleventyConfig.addFilter('excerpt', function(text, length = 100) {
-      if (text.length > length) {
-        return text.substring(0, length) + '...';
+  // Add a filter called "excerpt" to create a shortened version of a text
+  eleventyConfig.addFilter('excerpt', function(text, length = 100) {
+    if (text.length > length) {
+      return text.substring(0, length) + '...';
+    }
+    return text;
+  });
+
+  // syntax highlight
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    // Line separator for line breaks
+    lineSeparator: "\n",
+
+    // Change which Eleventy template formats use syntax highlighters
+    templateFormats: ["*"], // default
+
+    // Use only a subset of template types (11ty.js added in v4.0.0)
+    // templateFormats: ["liquid", "njk", "md", "11ty.js"],
+
+    // init callback lets you customize Prism
+    init: function({ Prism }) {
+      Prism.languages.myCustomLanguage = { /* … */ };
+    },
+
+    // Added in 3.1.1, add HTML attributes to the <pre> or <code> tags
+    preAttributes: {
+      tabindex: 0,
+
+      // Added in 4.1.0 you can use callback functions too
+      "data-language": function({ language, content, options }) {
+        return language;
       }
-      return text;
-    });
+    },
+    codeAttributes: {},
 
-    // syntax highlight
-    const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
-    module.exports = function (eleventyConfig) {
-      eleventyConfig.addPlugin(syntaxHighlight);
-    };
-
-    
+    // Added in 5.0.0, throw errors on invalid language names
+    errorOnInvalidLanguage: false,
+  });
 
   return {
     dir: {
@@ -33,4 +55,4 @@ module.exports = function(eleventyConfig) {
       output: "public"
     }
   };
-}
+};
